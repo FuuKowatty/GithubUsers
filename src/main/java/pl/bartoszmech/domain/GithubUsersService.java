b7908d598a0761afab535b70ab6776f1844e7de1
FuuKowatty <miedzio.2000@gmail.com>
2024-02-09 18:56:40 +0100

    refactor: add checkBestThreadNumber func and thread safe adding to List

--- src/main/java/pl/bartoszmech/domain/GithubUsersService.java
+++ src/main/java/pl/bartoszmech/domain/GithubUsersService.java
@@ -16,32 +16,36 @@ import java.util.concurrent.TimeUnit;
 @AllArgsConstructor
 public class GithubUsersService {
 
+    public static final long THREADS_WAITING_TIME = 3;
     public IFetcher fetcher;
 
     public List<ClientResponse> findAllRepositoriesByUsername(String username) {
-        long start = System.currentTimeMillis();
         List<RepositoriesResponseAPI> userRepositories = makeRequestForUserRepositories(username);
 
         List<ClientResponse> clientData = new LinkedList<>();
-        ExecutorService executorService = Executors.newFixedThreadPool(userRepositories.size());
+        ExecutorService executorService = Executors.newFixedThreadPool(checkBestThreadNumber(userRepositories));
 
-        userRepositories.forEach(repository -> {
-            executorService
-                .execute(() -> {
+        userRepositories.forEach(repository -> { executorService.execute(() -> {
                     List<BranchesResponseAPI> repositoryBranches = makeRequestForRepositoryBranches(repository.owner().login(), repository.name());
                     ClientResponse clientResponse = MapperResponseAPI.mapToClientResponse(repository, repositoryBranches);
-                    clientData.add(clientResponse);
+                    synchronized (clientData) {
+                        clientData.add(clientResponse);
+                    }
                 });
         });
 
         executorService.shutdown();
         try {
-            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            executorService.awaitTermination(THREADS_WAITING_TIME, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
-            //
+            throw new RuntimeException(e);
         }
-        System.out.println("Time taken: " + (System.currentTimeMillis() - start) + " ms");
+
         return clientData;
+    }
+
+    private static int checkBestThreadNumber(List<RepositoriesResponseAPI> userRepositories) {
+        return Math.min(userRepositories.size(), Runtime.getRuntime().availableProcessors());
     }
 
     private List<RepositoriesResponseAPI> makeRequestForUserRepositories(String username) {

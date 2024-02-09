aa11ceb6f5a9c8c410f79dc1b956f3c581926ca6
FuuKowatty <miedzio.2000@gmail.com>
2024-02-09 12:47:21 +0100

    feat: add spring-boot

---
+++ src/main/java/pl/bartoszmech/GithubUsers.java
@@ -0,0 +1,12 @@
+package pl.bartoszmech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GithubUsers {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubUsers.class, args);
+    }
+}
--- src/main/java/pl/bartoszmech/Main.java
+++
@@ -1,7 +0,0 @@
-package pl.bartoszmech;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}

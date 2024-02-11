425d20d6f885de213089f3f9f5a8a88a2a384626
FuuKowatty <miedzio.2000@gmail.com>
2024-02-11 19:34:41 +0100

    feat: add to swagger error responses

--- src/main/java/pl/bartoszmech/application/controller/GithubUsersController.java
+++ src/main/java/pl/bartoszmech/application/controller/GithubUsersController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.bartoszmech.application.response.ErrorResponseExternalAPI;
 import pl.bartoszmech.application.response.GithubUsersResponse;
 import pl.bartoszmech.domain.GithubUsersService;
 
@@ -24,13 +25,20 @@ public class GithubUsersController {
     private final GithubUsersService githubUsersService;
 
     @Operation(summary = "Retrieve data about user's repositories by specified username")
-    @ApiResponses(
-        @ApiResponse(
-            responseCode = "200", description = "Successful operation",
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful operation",
             content = @Content(mediaType = "application/json",
-            array =  @ArraySchema(schema = @Schema(implementation = GithubUsersResponse.class)))
-        )
-    )
+                array = @ArraySchema(schema = @Schema(implementation = GithubUsersResponse.class)))),
+        @ApiResponse(responseCode = "403", description = "It occurs propably because your API limit has exceeded",
+            content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponseExternalAPI.class))),
+        @ApiResponse(responseCode = "404", description = "Username not found",
+            content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponseExternalAPI.class))),
+        @ApiResponse(responseCode = "500", description = "External API server error",
+            content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponseExternalAPI.class))),
+    })
     @GetMapping("/{username}")
     public List<GithubUsersResponse> findAllRepositoriesByUsername(@PathVariable String username) {
         return githubUsersService.findAllRepositoriesByUsername(username);
